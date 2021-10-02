aab8b4176a0cd505b64f1a70d6174f5830da34d6
coder3223@gmail.com <coder3223@gmail.com>
2021-10-02 17:20:49 -0400

    moving files issues fix...I hope...

--- website_backend.py
+++ website_backend.py
@@ -1,10 +1,5 @@
 from flask import Flask, render_template, request
 import requests
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> a4ffd101371c286dc6a7ee35a7cb997e289a3e0d
 import smtplib
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
@@ -27,11 +22,9 @@ class ContactForm(FlaskForm):
 
 
 MY_EMAIL = os.environ.get("SITE_EMAIL")
+print(MY_EMAIL)
 PASSWORD = os.environ.get("EMAIL_PASSWORD")
-<<<<<<< HEAD
->>>>>>> a4ffd10... login and email validators now working
-=======
->>>>>>> a4ffd101371c286dc6a7ee35a7cb997e289a3e0d
+print(PASSWORD)
 
 app = Flask(__name__)
 app.secret_key = "ThisIsGold"
@@ -52,31 +45,6 @@ def projects():
     return render_template('projects.html')
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-@app.route('/contact')
-=======
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    login_form = LoginForm()
-    login_form.validate_on_submit()
-    return render_template('login.html', form=login_form)
-
-
-@app.route('/contact', methods=['GET', 'POST'])
->>>>>>> a4ffd101371c286dc6a7ee35a7cb997e289a3e0d
-def contact():
-    contact_form = ContactForm()
-    if request.method == 'POST':
-        if contact_form.validate_on_submit():
-            data = request.form
-            send_email(data["name"], data["email"], data["message"])
-            return render_template('contact.html', msg_sent=True, form=contact_form)
-    return render_template('contact.html', msg_sent=False, form=contact_form)
-
-
-<<<<<<< HEAD
-=======
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
@@ -95,25 +63,19 @@ def contact():
     return render_template('contact.html', msg_sent=False, form=contact_form)
 
 
-=======
->>>>>>> a4ffd101371c286dc6a7ee35a7cb997e289a3e0d
 def send_email(name, email, message):
     email_message = f"subject:NewMessage\n\nName: {name}\nEmail: {email}\nMessage: {message}"
     with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
         connection.starttls()
-        connection.login(str(MY_EMAIL), str(PASSWORD))
+        connection.login(str((MY_EMAIL)), str((PASSWORD)))
         connection.sendmail((MY_EMAIL), (MY_EMAIL), email_message)
-<<<<<<< HEAD
->>>>>>> a4ffd10... login and email validators now working
-=======
->>>>>>> a4ffd101371c286dc6a7ee35a7cb997e289a3e0d
 
 
 @app.route('/blog')
 def blog():
-    blog_url = "https://api.npoint.io/67e7562537ada3f5a874"
-    response = requests.get(blog_url)
-    all_posts = response.json()
+    blog_url="https://api.npoint.io/67e7562537ada3f5a874"
+    response=requests.get(blog_url)
+    all_posts=response.json()
     return render_template('blog.html', posts=all_posts)
 
 
